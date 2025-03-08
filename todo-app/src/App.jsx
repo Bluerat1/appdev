14b29fbddd1714ba1b12ea1416d3678997cce5f0
bluerat1 <delez.chamberlain@gmail.com>
2025-03-08 21:57:16 +0800

    reach todo enhance

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TodoList from "./TodoList";
+
+
 function App() {
-return (
-<div>
-<h1> React TODO App</h1>
-<TodoList />
+    return (
+    <div>
+        <h1> React TODO App</h1>
+        <TodoList />
 </div>
 );
 }
